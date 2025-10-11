d41ac2e42f530ba97162dbd772bdc729cf68fe50
abhay panara <69295627+panara12@users.noreply.github.com>
2025-10-12 01:55:52 +0530

    Update sessionlodder.js

--- backend/utils/sessionlodder.js
+++ backend/utils/sessionlodder.js
@@ -2,7 +2,6 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo");
 
 const mongoUrl = process.env.MONGODB_URL + "user_master";
-const isProduction = process.env.NODE_ENV === "production";
 
 app.use(session({
   secret: process.env.SESSION_SECRET || "fallbackSecretKey",
@@ -16,7 +15,7 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60, // 1 hour
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? "none" : "lax", // "none" for cross-site in production
+    secure: true,
+    sameSite: "none", // "none" for cross-site in production
   },
 }));
